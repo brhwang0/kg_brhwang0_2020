0562ba89a26a489ad55b552985c23bab30e9bdca
brhwang0 <brhwang@protonmail.com>
2020-03-13 21:38:30 -0400

    Update comments, clean line spacing

--- Main.java
+++ Main.java
@@ -1,22 +1,8 @@
-/**
-
-   Determine whether a one-to-one character mapping 
-   exists from one string, s1, to another string, s2.
-   For example, given ​s1 = abc​ and ​s2 = bcd​, print 
-   "​true" into stdout​ since we can map each character 
-   in "abc" to a character in "bcd".Given ​s1 = foo​ and ​
-   s2 = bar​, print "​false"​ since the character ‘o’ cannot 
-   map to two characters.But given ​s1 = bar ​and ​s2 = foo​, 
-   print "true​"​ since each character in "bar" can be mapped 
-   o acharacter in "foo".
-
-**/
-
 class Main {
 
 	public static boolean check(String s1, String s2) {
 		
-		// Create two strings of distinct characters in s1, s2
+		// Create string of distinct characters in s1
 		String distinctS1 = "";
 		for (int i = 0; i < s1.length(); i++) {
 			if (distinctS1.indexOf(s1.charAt(i)) == -1) {
@@ -31,7 +17,6 @@ class Main {
 
 	public static void main(String[] args) {
 		try {
-			
 			// Catch input error
 			if (args.length != 2) {
 				System.out.println("Input error. Please enter only two strings.");
@@ -40,12 +25,9 @@ class Main {
 				String in2 = args[1];
 				System.out.println(check(in1, in2));
 			}
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
-	
 	
 }
